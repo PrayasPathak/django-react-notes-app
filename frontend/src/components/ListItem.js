86fb11e44a5aad91acdcdcd42f8c70a60f4e05b1
PrayasPathak <prayasroman1@gmail.com>
2024-04-07 19:15:53 +0545

    generating timestamp for last update

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getTimeStamp = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
 const ListItem = ({ note }) => {
   return (
     <div>
       <Link to={`/notes/${note.id}`}>
         <div className="notes-list-item">
           <h3>{note.title}</h3>
+          <p>
+            Last Updated:
+            <span>{getTimeStamp(note)}</span>
+          </p>
         </div>
       </Link>
     </div>
